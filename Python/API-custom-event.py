b10cb3d22e1f2b9800faa14bbe8e29482fcc86b2
Diggory Rycroft <diggory@mixpanel.com>
2015-03-03 19:11:22 -0800

    Adding API-custom-event.py

---
+++ Python/API-custom-event.py
@@ -0,0 +1,53 @@
+import time
+import urllib
+import md5
+import requests
+import json
+
+expire = int(time.time()) + 1000
+
+def make_sig(raw_url):
+	url = urllib.unquote(raw_url).decode('utf8')
+	url = url.replace("+"," ")
+	args = url.split("?")[1].split("&")
+	args_new = []
+
+	for i in args:
+		if i[0:4] != "sig=":
+			args_new.append(i)
+	args_concat = "".join(sorted(args_new))
+	sig = md5.new(args_concat+api_secret).hexdigest()
+	return raw_url + "&sig=" + sig
+
+def collect_events():
+	addMore = True
+	alternatives = []
+	while addMore:
+		altEvent = raw_input("Add an Event to this Custom Event:  ")
+		event = {}
+		event["event"] = altEvent
+		alternatives.append(event)
+		addMoreQ = raw_input("Thanks! Add another? (Y/N)  ")
+		if addMoreQ.lower() != "y":
+			addMore = False
+
+	alternatives = json.dumps(alternatives)
+	print alternatives
+	return alternatives
+
+def create_custom_event():
+	root = "https://mixpanel.com/api/2.0/custom_events/create/"
+	URL = "%s?expire=%d&api_key=%s&name=%s&alternatives=%s" % (root, expire, api_key, name, alternatives)
+	URL = make_sig(URL)
+	print "Sending request to %s ..." % (URL)
+	print "\n\n'%s' custom event created!" % (name)
+	response = requests.post(URL).text
+	return response
+
+api_key = "3b117b22cd31c800465239af00a778c5" #raw_input("Enter API Key:  ")
+api_secret = "2b1c88c6b8e6fc397b1928d36e6c0a4d" #raw_input("Enter API Secret:  ")
+token = "68436c8f473803cf42dfc1c554a82149" #raw_input("Enter Project Token:  ")
+name = raw_input("What would you like to call the custom event?  ")
+
+alternatives = collect_events()
+print create_custom_event()
